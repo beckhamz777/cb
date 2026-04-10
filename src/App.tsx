c33eb4c1dd5bab804b5107fe02e7a4ad580c584e
Beckhamz777 <oworinawebeckham17@gmail.com>
2026-04-10 13:32:30 +0300

    fix: switch to hash router for correct direct link routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar, Footer } from './components/Layout';
 import Home from './pages/Home';
 import WindowsPage from './pages/WindowsPage';
